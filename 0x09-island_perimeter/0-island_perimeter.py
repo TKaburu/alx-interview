53201fd13ae45d7d756a3185a9cd53e67e98bb56
TKaburu <traceymwendwa@gmail.com>
2024-06-26 20:05:04 +0300

    feat: Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+def island_perimeter(grid):
+    """
+    This function calculates the boder/perimeter of an island
+    """
+    rows = len(grid)
+    # check if the grid is not empty
+    if rows > 0:
+        # set colums if grid is not empty
+        columns = len(grid[0])
+    else:
+        columns = 0
+
+    perimeter = 0  # will keep track of the total perimeter of the island
+
+    # land == 1 water == 0
+    for row in range(rows):
+        for column in range(columns):
+            if grid[row][column] == 1:
+                # we add 4 cause an island has 4 sides
+                perimeter = perimeter + 4
+                # check if above is a land
+                if row > 0 and grid[row-1][column] == 1:
+                    # minu 2 because we have 2 boders side by side since
+                    # both are land
+                    perimeter = perimeter - 2
+                # checks if to the left is land
+                if column > 0 and grid[row][column-1] == 1:
+                    perimeter = perimeter - 2
+
+    return perimeter
